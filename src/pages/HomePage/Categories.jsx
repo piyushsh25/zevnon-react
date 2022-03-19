34bcb5240eaf1b5e933ca114002de388856d4cfa
piyushpoudyal <72690317+piyushsh25@users.noreply.github.com>
2022-03-19 17:07:29 +0530

    Revert "Revert "navbar+ home page""
    This reverts commit a3d890ad51e700b2bb0c4eb1a851a23fa47d8203.

---
+++ src/pages/HomePage/Categories.jsx
@@ -0,0 +1,120 @@
+import "../../styles/Categories.css"
+export const Categories=()=>{
+    return(
+        <div className="shopping-section">
+        <div className="card text-overlay-card">
+            <div className="img-div">
+                <img src="https://rukminim2.flixcart.com/image/832/832/kw9krrk0/headphone/z/i/p/-original-imag8z6ht9qffnju.jpeg?q=70" alt="product-img"/>
+            </div>
+            <div className="text-div">
+                <div className="header-top">Min 30% off</div>
+                <div className="header-bottom"> Electronics</div>
+                <div className="description">All phone models, Desktop, Soundbox, DSLRs, Keyboard, Webcams, Data Card
+                </div>
+                <ul>
+                    <li className="card-icons text-icon"><a href="./docs/pages/listing.html">Shop</a></li>
+
+
+                    <li className="card-icons icons"> <i className="lni lni-share-alt-1"></i></li>
+                    <li className="card-icons icons"><i className="lni lni-more-alt"></i></li>
+                </ul>
+            </div>
+
+        </div>
+        <div className="card text-overlay-card">
+            <div className="img-div">
+                <img src="https://rukminim2.flixcart.com/image/832/832/jqqy6q80/sofa-sectional/n/n/4/grey-chenille-nano-gr-blu-6-bharat-lifestyle-blue-grey-original-imafcp9fnhkhpwa4.jpeg?q=70" alt="product-img"/>
+            </div>
+            <div className="text-div">
+                <div className="header-top">Min 30% off</div>
+                <div className="header-bottom">Home Appliances</div>
+                <div className="description">Living Room Kitchen & Dining Bedroom room Home Decor Tools & Utility.
+                </div>
+                <ul>
+                    <li className="card-icons text-icon"><a href="./docs/pages/listing.html">Shop</a></li>
+                   
+                    <li className="card-icons icons"> <i className="lni lni-share-alt-1"></i></li>
+                    <li className="card-icons icons"><i className="lni lni-more-alt"></i></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div className="card text-overlay-card">
+            <div className="img-div">
+                <img src="https://rukminim2.flixcart.com/image/300/300/kjhgzgw0-0/shoe/j/2/b/194073-8-puma-white-peacoat-original-imafzffyxzz59yb7.jpeg?q=70" alt="Sports shoes,clogs, sandals and more"/>
+            </div>
+            <div className="text-div">
+                <div className="header-top">Min 40% off</div>
+                <div className="header-bottom">Footwear</div>
+                <div className="description">Sports shoes,clogs, sandals and more..Sports shoes,clogs, sandals and more..Sports e
+                </div>
+                <ul>
+                    <li className="card-icons text-icon"><a href="./docs/pages/listing.html">Shop</a></li>
+
+                
+                    <li className="card-icons icons"> <i className="lni lni-share-alt-1"></i></li>
+                    <li className="card-icons icons"><i className="lni lni-more-alt"></i></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div className="card text-overlay-card">
+            <div className="img-div">
+                <img src="https://rukminim2.flixcart.com/image/300/300/kgsb1jk0/remote-control-toy/5/z/w/1-18-very-high-speed-race-car-style-4-channel-radio-control-rc-original-imafwxsggzsczg3u.jpeg?q=70" alt="product-img"/>
+            </div>
+            <div className="text-div">
+                <div className="header-top">Min 30% off</div>
+                <div className="header-bottom">Books and stationary</div>
+                <div className="description"> Key Chains, Erasers, Soft Toys, Card Holder, Pens, Desk Organisers, Outdoor tous, files and folders
+                </div>
+                <ul>
+                    <li className="card-icons text-icon"><a href="./docs/pages/listing.html">Shop</a></li>
+
+                    <li className="card-icons icons"> <i className="lni lni-share-alt-1"></i></li>
+                    <li className="card-icons icons"><i className="lni lni-more-alt"></i></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div className="card text-overlay-card">
+            <div className="img-div">
+                <img src="https://rukminim2.flixcart.com/image/612/612/kka1si80/aerated-drink/x/4/o/strawberry-non-alcoholic-beer-330ml-pack-of-6-glass-bottle-original-imafznzff7dvdzu3.jpeg?q=70" alt="product-img"/>
+            </div>
+            <div className="text-div">
+                <div className="header-top">Min 30% off</div>
+                <div className="header-bottom">Food & Beverages</div>
+                <div className="description">Nuts & Dry fruits, Jams, Spreads and honey. Snacks & nibbles and more...Nuts & Dry fruits, Jams, Spreads and honey.
+                </div>
+                <ul>
+                    <li className="card-icons text-icon"><a href="./docs/pages/listing.html">Shop</a></li>
+
+                    <li className="card-icons icons"> <i className="lni lni-share-alt-1"></i></li>
+                    <li className="card-icons icons"><i className="lni lni-more-alt"></i></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div className="card text-overlay-card">
+            <div className="img-div">
+                <img src="https://rukminim2.flixcart.com/image/612/612/keg02a80/fmcg-combo/u/f/v/dailydose-200g-super-healthy-original-imafv4ggnwmqkw3k.jpeg?q=70" alt="product-img"/>
+            </div>
+            <div className="text-div">
+                <div className="header-top">Min 30% off</div>
+                <div className="header-bottom">Grocery</div>
+                <div className="description">Cookies, Maggi, Mixed fruit Juice, Corn flakes, ,Chocos, tooth paste, Dry fruits,Cookies, Maggi, Mixed fruit Juice.
+                </div>
+                <ul>
+                    <li className="card-icons text-icon"><a href="./docs/pages/listing.html">Shop</a></li>
+                    <li className="card-icons icons"> <i className="lni lni-share-alt-1"></i></li>
+                    <li className="card-icons icons"><i className="lni lni-more-alt"></i></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+    )
+}
