265447b011251f708961bda1b97cb1b8a3cb74ea
piyushpoudyal <72690317+piyushsh25@users.noreply.github.com>
2022-03-20 11:51:37 +0530

    added some images in categories section

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -15,18 +15,18 @@ export const categories = [
     categoryName: "footwear",
     categoryInfo:" 30% off",
     description:
-    "Buy online at Footwear. Huge footwear and Clothing range. Order now and avail Best D",
+    "Buy online at Footwear. Huge footwear and Clothing range. Order now and avail Best Discount",
     
-    categoryImage:"https://rukminim2.flixcart.com/image/832/832/kw9krrk0/headphone/z/i/p/-original-imag8z6ht9qffnju.jpeg?q=70"
+    categoryImage:"https://rukminim2.flixcart.com/image/300/300/kjhgzgw0-0/shoe/j/2/b/194073-8-puma-white-peacoat-original-imafzffyxzz59yb7.jpeg?q=70"
   },
   {
     _id: uuid(),
     categoryName: "bagpack",
     categoryInfo:"Buy 3+1 free",
     description:
-    "Buy online at Footwear. Huge footwear and Clothing range. Order now and avail Best D"
+    "Buy online at Footwear. Huge footwear and Clothing range. Order now and avail Best Discount"
     ,
-    categoryImage:"https://rukminim2.flixcart.com/image/832/832/kw9krrk0/headphone/z/i/p/-original-imag8z6ht9qffnju.jpeg?q=70"
+    categoryImage:"https://rukminim2.flixcart.com/image/300/300/kgsb1jk0/remote-control-toy/5/z/w/1-18-very-high-speed-race-car-style-4-channel-radio-control-rc-original-imafwxsggzsczg3u.jpeg?q=70"
   },
   {
     _id: uuid(),
