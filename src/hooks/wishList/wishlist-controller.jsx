dcd3bd5ced0c26e6c3b80e653efcdf0136eff622
piyushpoudyal <72690317+piyushsh25@users.noreply.github.com>
2022-03-31 20:34:45 +0530

    bug: fixed wishlist button in listing page.

--- src/hooks/wishList/wishlist-controller.jsx
+++ src/hooks/wishList/wishlist-controller.jsx
@@ -1,47 +1,43 @@
-export function wishReducerFunction(state,action){
- 
-    switch (action.type){
+export function wishReducerFunction(state, action) {
+
+    switch (action.type) {
         case "wishlist":
-            let productInWishList=false;
-            let updatedWishList=state.wishItems.map((items)=>{
-                if(action.payload.id===items.id){
-                    productInWishList=true;
+            let productInWishList = false;
+            let updatedWishList = state.wishItems.map((items) => {
+                if (action.payload.id === items.id) {
+                    productInWishList = true;
                     return items;
                 }
                 return items;
             })
-            const {id,title,img,price,discountedPrice}=action.payload
-            if(!productInWishList){
-                updatedWishList=[...state.wishItems,{id,title,img,price,discountedPrice}]
-                return {...state,wishCount:state.wishCount+1,wishItems:updatedWishList,inWishlist:true}
+            const { id, title, img, price, discountedPrice } = action.payload
+            if (!productInWishList) {
+                updatedWishList = [...state.wishItems, { id, title, img, price, discountedPrice }]
+                return { ...state, wishCount: state.wishCount + 1, wishItems: updatedWishList, inWishlist: true }
             }
-            
-            return {...state}
-          case "remove":
-             
-              const removedItems=state.wishItems.filter((items)=> {
-                console.log(state);
-                  if (items.id===action.payload.id){
-                      return  action.payload!==items
-                  }
-                  return items
-              })
-              return {...state,wishItems:removedItems,wishCount:state.wishCount-1,addToishListMessage:false,inWishlist:false}
-       default:
-           return {...state}
-    }
-  }
 
- export function wishListHandler(product,wishState, wishDispatch) {
+            return { ...state }
+        case "remove":
+
+            const removedItems = state.wishItems.filter((items) => {
+                if (items.id === action.payload.id) {
+                    return action.payload !== items
+                }
+                return items
+            })
+            return { ...state, wishItems: removedItems, wishCount: state.wishCount - 1, addToishListMessage: false, inWishlist: false }
+        default:
+            return { ...state }
+    }
+}
+
+export function wishListHandler(product, wishState, wishDispatch) {
     let addToishListMessage = false;
     wishState.wishItems.some((items) => {
         if (items.id === product.id) {
             addToishListMessage = true;
             wishDispatch({ type: "remove", payload: items })
-
-            return items;
         }
-        return items;
 
     })
     if (!addToishListMessage) {
